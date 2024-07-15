145ab6812b376ee8999c5bbcbb6e747d0e709828
Jade1724 <haruka.i1724@gmail.com>
2024-07-16 09:16:33 +1200

    feat: get all books endpoint.

--- books.py
+++ books.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'maths'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'fiction'},
+]
+
 @app.get("/")
 async def first_api():
     return {"message": "Hello FastAPI!"}
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
