648c5fc05e60c49ee3155b71c1c3f8fb3b0f2d8b
Jade1724 <haruka.i1724@gmail.com>
2024-07-16 09:33:52 +1200

    feat: path parameter.

--- books.py
+++ books.py
@@ -18,3 +18,17 @@ async def first_api():
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/mybook")
+async def read_my_book():
+    return {'book_title': 'Title Two'}
+
+
+@app.get("/books/{book_title}")
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
