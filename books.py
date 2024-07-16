4821c5faa010f1e0b44dd5927f5fb67432385048
Jade1724 <haruka.i1724@gmail.com>
2024-07-17 09:30:17 +1200

    feat: query parameter get request.

--- books.py
+++ books.py
@@ -24,11 +24,28 @@ async def read_all_books():
 async def read_my_book():
     return {'book_title': 'Title Two'}
 
-
+# Path parameter example
 @app.get("/books/{book_title}")
 async def read_all_books(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
 
+# Query parameter example
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
 
+
+# Path parameter & query parameter example
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category:str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
